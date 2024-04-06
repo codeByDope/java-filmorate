96d4e70936bcac2aec505ca793948d165c801b3c
Likholetov Kirill <shotykrd@yandex.ru>
2024-04-06 18:25:31 +0300

    fix: test

--- src/test/java/ru/yandex/practicum/filmorate/LikerDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/LikerDbStorageTest.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.mapper.FilmRowMapper;
 import ru.yandex.practicum.filmorate.mapper.GenreRowMapper;
 import ru.yandex.practicum.filmorate.mapper.MpaRatingRowMapper;
 import ru.yandex.practicum.filmorate.mapper.UserRowMapper;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
@@ -23,6 +22,7 @@ import ru.yandex.practicum.filmorate.storage.rating.RatingStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,34 +65,27 @@ public class LikerDbStorageTest {
         jdbcTemplate.execute("MERGE INTO mpa_ratings(id, title) VALUES (3, 'PG-13')");
         jdbcTemplate.execute("MERGE INTO mpa_ratings(id, title) VALUES (4, 'R')");
         jdbcTemplate.execute("MERGE INTO mpa_ratings(id, title) VALUES (5, 'NC-17')");
-    }
-
-    @Test
-    public void testLiker() {
-        Film film = new Film.Builder()
-                .id(1L)
-                .name("Test Film")
-                .description("Test Film Description")
-                .releaseDate(LocalDate.of(2023, 1, 1))
-                .duration(120)
-                .genres(genreStorage.getAll())
-                .mpa(ratingStorage.getById(1).get())
-                .build();
-
-        filmStorage.add(film);
 
         User newUser = new User(1L, "user1@email.ru", "vanya1", "Ivan Petrov", LocalDate.of(1991, 1, 1));
         User newUser1 = new User(2L, "user2@email.ru", "vanya2", "Ivan Petrov", LocalDate.of(1992, 2, 2));
         User newUser2 = new User(3L, "user3@email.ru", "vanya3", "Ivan Petrov", LocalDate.of(1993, 3, 3));
+        String sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, newUser.getId(), newUser.getEmail(), newUser.getLogin(), newUser.getName(), Date.valueOf(newUser.getBirthday()));
+        jdbcTemplate.update(sql, newUser1.getId(), newUser1.getEmail(), newUser1.getLogin(), newUser1.getName(), Date.valueOf(newUser1.getBirthday()));
+        jdbcTemplate.update(sql, newUser2.getId(), newUser2.getEmail(), newUser2.getLogin(), newUser2.getName(), Date.valueOf(newUser2.getBirthday()));
 
-        userStorage.add(newUser);
-        userStorage.add(newUser1);
-        userStorage.add(newUser2);
+        jdbcTemplate.update("MERGE INTO films(id, title, description, release_date, duration, rating_id) VALUES (?, ?, ?, ?, ?, ?)",
+                1L, "Test Film", "Test Film Description", LocalDate.of(2023, 1, 1), 120, 1L);
 
-        likerStorage.add(1L, 1L);
-        likerStorage.add(1L, 2L);
-        likerStorage.add(1L, 3L);
+        String sqlLiker = "INSERT INTO likers VALUES(?,?);";
+        jdbcTemplate.update(sqlLiker, 1L, 1L);
+        jdbcTemplate.update(sqlLiker, 1L, 2L);
+        jdbcTemplate.update(sqlLiker, 1L, 3L);
 
+    }
+
+    @Test
+    public void testLiker() {
         assertEquals(3, likerStorage.getLikersByFilmId(1L).size());
     }
 }
