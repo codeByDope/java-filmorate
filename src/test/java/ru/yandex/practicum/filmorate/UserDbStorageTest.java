0444766f9cc457f4eba091833a0156689961d9cf
Likholetov Kirill <shotykrd@yandex.ru>
2024-04-06 18:13:04 +0300

    fix: test

--- src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import ru.yandex.practicum.filmorate.mapper.UserRowMapper;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -22,11 +24,22 @@ class UserDbStorageTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        loadTestData();
+    }
+
+    private void loadTestData() {
+        user = new User(1L, "user@email.ru", "vanya123", "Ivan Petrov", LocalDate.of(1990, 1, 1));
+        String sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getId(), user.getEmail(), user.getLogin(), user.getName(), Date.valueOf(user.getBirthday()));
+    }
+
     @Test
     public void testFindUserById() {
-        User newUser = new User(1L, "user@email.ru", "vanya123", "Ivan Petrov", LocalDate.of(1990, 1, 1));
         UserDbStorage userStorage = new UserDbStorage(new UserRowMapper(), jdbcTemplate);
-        userStorage.add(newUser);
 
         Optional<User> savedUserOptional = userStorage.getById(1L);
 
@@ -35,10 +48,10 @@ class UserDbStorageTest {
         User savedUser = savedUserOptional.get();
 
         assertThat(savedUser).isNotNull();
-        assertThat(savedUser.getId()).isEqualTo(newUser.getId());
-        assertThat(savedUser.getEmail()).isEqualTo(newUser.getEmail());
-        assertThat(savedUser.getLogin()).isEqualTo(newUser.getLogin());
-        assertThat(savedUser.getName()).isEqualTo(newUser.getName());
-        assertThat(savedUser.getBirthday()).isEqualTo(newUser.getBirthday());
+        assertThat(savedUser.getId()).isEqualTo(user.getId());
+        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(savedUser.getLogin()).isEqualTo(user.getLogin());
+        assertThat(savedUser.getName()).isEqualTo(user.getName());
+        assertThat(savedUser.getBirthday()).isEqualTo(user.getBirthday());
     }
 }
