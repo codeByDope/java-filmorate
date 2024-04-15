38bff20955f5573339e8fa49fd330852a7ae73ab
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-15 17:18:32 +0300

    feat: добавил контроллер для обработки запроса ленты событий

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.User;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class FeedController {
+
+
+    @GetMapping("/{id}/feed")
+    public List<Feed> getUserById(@PathVariable Long id) {
+        log.info("Был запрошен пользователь с ID");
+//        return service.getUserById(id);
+        return null;
+    }
+}
