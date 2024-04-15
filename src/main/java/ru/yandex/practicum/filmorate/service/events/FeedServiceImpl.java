65babf4d28fef37c6ec83ec74bb9f88e5775402c
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-15 17:35:37 +0300

    feat: реализация FeedService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/events/FeedServiceImpl.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.service.events;
+
+import ru.yandex.practicum.filmorate.model.Event;
+
+import java.util.List;
+
+public class FeedServiceImpl implements FeedService{
+    @Override
+    public List<Event> getFeed() {
+        return List.of();
+    }
+}
