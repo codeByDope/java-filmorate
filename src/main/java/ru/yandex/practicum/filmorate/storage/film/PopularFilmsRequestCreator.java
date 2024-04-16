70ad9d44a5d308c10f3c07ac2f219eb168ec8585
iorlova <rinorlova@gmail.com>
2024-04-16 19:29:58 +0200

    [fix] fix after postman checks

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/PopularFilmsRequestCreator.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/PopularFilmsRequestCreator.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class PopularFilmsRequestCreator {
 
-    private static final String MOST_POPULAR_FILMS_REQUEST_BASIS = "SELECT f.* " +
+    private static final String MOST_POPULAR_FILMS_REQUEST_BASIS = "SELECT f.*, MAX(top_films.count_likes) " +
             "FROM films AS f " +
             "LEFT JOIN films_genres fg ON f.id = fg.film_id " +
             "LEFT JOIN (" +
             "SELECT film_id, COUNT(liker_id) AS count_likes " +
             "FROM likers " +
             "GROUP BY film_id " +
-            "ORDER BY count_likes DESC " +
             ") AS top_films ON f.id = top_films.film_id ";
+    private static final String ORDER_BY_STATEMENT = "group by f.id ORDER BY top_films.count_likes DESC ";
     private static final String AND_QUERY_STATEMENT = " AND ";
     private static final String WHERE_QUERY_STATEMENT = " WHERE ";
     private static final String LIMIT_QUERY_STATEMENT = " LIMIT %s ";
@@ -31,6 +31,7 @@ public class PopularFilmsRequestCreator {
                 .append(String.format(YEAR_QUERY_CONDITION, year))
                 .append(AND_QUERY_STATEMENT)
                 .append(String.format(GENRE_ID_CONDITION, genreId))
+                .append(ORDER_BY_STATEMENT)
                 .append(String.format(LIMIT_QUERY_STATEMENT, count))
                 .toString();
     }
@@ -38,6 +39,7 @@ public class PopularFilmsRequestCreator {
     public String createMostPopularFilmsQuery(Long count) {
         return new StringBuilder()
                 .append(MOST_POPULAR_FILMS_REQUEST_BASIS)
+                .append(ORDER_BY_STATEMENT)
                 .append(String.format(LIMIT_QUERY_STATEMENT, count))
                 .toString();
 
@@ -49,6 +51,7 @@ public class PopularFilmsRequestCreator {
                 .append(MOST_POPULAR_FILMS_REQUEST_BASIS)
                 .append(WHERE_QUERY_STATEMENT)
                 .append(String.format(GENRE_ID_CONDITION, genreId))
+                .append(ORDER_BY_STATEMENT)
                 .append(String.format(LIMIT_QUERY_STATEMENT, count))
                 .toString();
     }
@@ -59,6 +62,7 @@ public class PopularFilmsRequestCreator {
                 .append(MOST_POPULAR_FILMS_REQUEST_BASIS)
                 .append(WHERE_QUERY_STATEMENT)
                 .append(String.format(YEAR_QUERY_CONDITION, year))
+                .append(ORDER_BY_STATEMENT)
                 .append(String.format(LIMIT_QUERY_STATEMENT, count))
                 .toString();
     }
