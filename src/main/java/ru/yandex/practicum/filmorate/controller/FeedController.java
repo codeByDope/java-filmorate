366160b70fcfed39e5cbdf31cdc0cf6b62bdd9aa
Kulkov_Mikhail <mk1221@yandex.ru>
2024-04-17 20:58:10 +0300

    вывел эндпоинты в ApiPathConstants

--- src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.controller.utils.ApiPathConstants;
 import ru.yandex.practicum.filmorate.model.Feed;
 import ru.yandex.practicum.filmorate.service.feed.FeedService;
 
@@ -13,12 +14,12 @@ import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping(ApiPathConstants.USERS_PATH)
 @RestController
 public class FeedController {
     private final FeedService service;
 
-    @GetMapping("/{id}/feed")
+    @GetMapping(ApiPathConstants.FEED_PATH)
     public List<Feed> getAll(@PathVariable Long id) {
         log.info("Запрос ленты событий для пользователя с Id {}", id);
         return service.getAllByUserId(id);
--- src/main/java/ru/yandex/practicum/filmorate/controller/utils/ApiPathConstants.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/utils/ApiPathConstants.java
@@ -7,7 +7,10 @@ public class ApiPathConstants {
     public static final String DIRECTORS_PATH = "/directors";
     public static final String BY_ID_PATH = "/{id}";
     public static final String REVIEW_PATH = "reviews";
+    public static final String USERS_PATH = "/users";
     public static final String LIKE_PATH = BY_ID_PATH + "/like/{userId}";
     public static final String DISLIKE_PATH = BY_ID_PATH + "/dislike/{userId}";
+    public static final String FEED_PATH = USERS_PATH + "/{id}/feed";
+
 
 }
